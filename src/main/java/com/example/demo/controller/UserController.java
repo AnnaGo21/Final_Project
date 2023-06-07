0baa2c955905e0734b2f983f355159533ec0032e
Ani <anikubana@gmail.com>
2023-06-07 21:01:48 +0400

    Modified UserController.java

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -20,7 +20,7 @@ public class UserController {
 
     @GetMapping("/user")
     public String getUser(){
-        return "Hello, user!";
+        return "User added. Hello, user!";
     }
 
     @GetMapping("/add")
